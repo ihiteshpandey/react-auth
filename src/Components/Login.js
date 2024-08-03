bc22087f722225a64cb46693c5a3e34b11e4aeb5
ihiteshpandey <hiteshpandey183@gmail.com>
2024-08-04 01:06:21 +0530

    Updates in UI

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -36,21 +36,22 @@ const Login = () => {
             <h2 className="text-2xl font-bold mb-2">Login</h2>
             </div>
             <form onSubmit={handleLogin}>
-    
-              <div className="mb-4">
-                <label className="block text-gray-700"> Your Email</label>
-                <input name='email' value={input.email} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
-                  type="text"
-                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700">Password</label>
-                <input name='password' value={input.password} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
-                  type="password"
-                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-              </div>
+            <div className='mt-8 '>
+                <label className='text-lg font-medium'>
+                    Email
+                </label>
+                <input 
+                name='email' value={input.email} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
+                className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your Email'/>
+            </div>
+            <div className='mt-8 '>
+                <label className='text-lg font-medium'>
+                    Password
+                </label>
+                <input
+                name='password' value={input.password} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
+                className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your Password' type="password"/>
+            </div>
               <button
                 type="submit"
                 className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-700"
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -23,45 +23,68 @@ const Register = () => {
         localStorage.setItem("user",JSON.stringify(input))
         navigate("/login");
     };
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <div className="text-center">
-        <h2 className="text-2xl font-bold mb-2">Register</h2>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700"> Your Name</label>
-            <input name='name' value={input.name} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
-              type="text"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            />
-          </div>
+    return(
+        <div className='bg-white px-10 py-20 rounded-3xl border-2 border-gray-200'>
+            <h1 className='text-5xl font-semibold'>
+                Register
+            </h1>
+            <p className='font-medium text-lg text-gray-500 mt-4 '>
+                Welcome! Please Enter Your Details.
+            </p>
+            <form onSubmit={handleSubmit}>
+            <div className='mt-8 '>
+                <label className='text-lg font-medium'>
+                    Name
+                </label>
+                <input 
+                name='name' value={input.name} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
+                className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your Email'/>
+            </div>
+            <div className='mt-8 '>
+                <label className='text-lg font-medium'>
+                    Email
+                </label>
+                <input 
+                name='email' value={input.email} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
+                className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your Email'/>
+            </div>
+            <div className='mt-8 '>
+                <label className='text-lg font-medium'>
+                    Password
+                </label>
+                <input
+                name='password' value={input.password} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
+                className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your Password' type="password"/>
+            </div>
+            <div className='flex mt-8 justify-between items-center'>
+                <div>
+                    <input type="checkbox" id='remember'/>
+                    <label className='ml-2 font-medium text-base' for="remember">
+                        Remember for 30 days
+                    </label>
+                </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700"> Your Email</label>
-            <input name='email' value={input.email} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
-              type="text"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Password</label>
-            <input name='password' value={input.password} onChange={(e) => setinput({...input,[e.target.name] : e.target.value})}
-              type="password"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-700"
-          >
-            Register
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+                <button className='font medium text-base text-violet-500'>
+                    Forgot Password
+                </button>
+
+            </div>
+            <div className='mt-8 flex flex-col gap-y-4'>
+                <button className=' active:scale-[.98 ] active:duration-75 hover:scale-[1.01] easy-in-out transition-all py-3 rounded-xl  bg-violet-500 text-white text-lg font-bold'>
+                    Register
+                </button>
+
+                <div className='mt-8 flex justify-center items-center'>
+                    <p className='font-medium text-base'> 
+                        Don't have an Account?
+                    </p>
+                    <button className='text-violet-500 text-medium ml-2'>Sign Up</button>
+                </div>
+
+            </div>
+         </form>
+        </div>
+    )
 };
 
 export default Register;
